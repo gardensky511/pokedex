b2084d518b37fce1c3a47a2ec8a06b4c2c30c452
jeongmin <luvmini511@gmail.com>
2021-05-29 16:42:23 +0900

    :tada: 環境構築

---
+++ src/components/pages/Home/HomePresentation.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const HomePresentation = () => <div>トップページ</div>;
---
+++ src/components/pages/Home/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { HomePresentation } from './HomePresentation';
+
+export const HomeContainer = () => (
+  <HomePresentation />
+);
