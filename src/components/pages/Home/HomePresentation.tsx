bd2686a65eae3e9bdabdf0083750700caabf85be
jeongmin <luvmini511@gmail.com>
2021-05-29 23:11:25 +0900

    :lipstick: List コンポーネント実装

--- src/components/pages/Home/HomePresentation.tsx
+++ src/components/pages/Home/HomePresentation.tsx
@@ -1,3 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FormattedPokemonList } from './declarations';
+import { ListCard } from '../../molecules/ListCard';
 
-export const HomePresentation = () => <div>トップページ</div>;
+type Props = {
+  pokemonList: FormattedPokemonList;
+};
+
+const List = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 15px;
+  background-color: #f1c40f;
+`;
+
+export const HomePresentation = ({ pokemonList }: Props) => {
+  return (
+    <List>
+      {pokemonList.map((pokemonListItem) => {
+        const { id, name, imageUrl } = pokemonListItem;
+        return <ListCard id={id} name={name} imageUrl={imageUrl} key={id} />;
+      })}
+    </List>
+  );
+};
--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { HomePresentation } from './HomePresentation';
+import { AppDispatch, useSelector } from '../../../redux/store';
+import { fetchPokemonList } from '../../../redux/modules/pokemonList';
+import { pokemonListSelector } from '../../../redux/selectors/pokemonList';
 
-export const HomeContainer = () => (
-  <HomePresentation />
-);
+export const HomeContainer = () => {
+  const pokemonList = useSelector(pokemonListSelector);
+  console.log(pokemonList);
+
+  const dispatch: AppDispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPokemonList());
+  }, []);
+
+  return <HomePresentation pokemonList={pokemonList} />;
+};
