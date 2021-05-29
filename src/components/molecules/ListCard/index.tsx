ea605010c531b6fdb24c9349403e3321ffa1ce7f
jeongmin <luvmini511@gmail.com>
2021-05-29 23:10:56 +0900

    :lipstick: ListCard コンポーネント実装

---
+++ src/components/molecules/ListCard/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  id: number;
+  name: string;
+  imageUrl: string;
+};
+
+const Container = styled.div`
+  background-color: #ecf0f1;
+  border-radius: 12px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 10px;
+`;
+
+const Name = styled.p`
+  font-weight: 900;
+  font-size: 16px;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Id = styled.div`
+  font-weight: 900;
+  font-size: 14px;
+`;
+
+export const ListCard = ({ id, name, imageUrl }: Props) => {
+  return (
+    <Container>
+      <Id>#{id}</Id>
+      <Image src={imageUrl} alt={name} />
+      <Name>{name[0].toUpperCase() + name.slice(1)}</Name>
+    </Container>
+  );
+};
