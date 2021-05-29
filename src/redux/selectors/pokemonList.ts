aa363405441870b668cdf03214c10982fbacdaf2
jeongmin <luvmini511@gmail.com>
2021-05-29 23:08:48 +0900

    :sparkles: pokemonListSelector の返り値フォーマット修正

--- src/redux/selectors/pokemonList.ts
+++ src/redux/selectors/pokemonList.ts
@@ -1,8 +1,15 @@
 import { createSelector } from 'reselect';
 import { RootState } from '../store';
-import { PokemonListState } from '../../components/pages/Home/declarations';
+import { PokemonList, FormattedPokemonList } from '../../components/pages/Home/declarations';
+import { getPokemonId, getPokemonImageURL } from '../../components/utils/function';
 
-export const pokemonListSelector = createSelector<RootState, PokemonListState, Array<{ name: string; url: string }>>(
-  (state) => state.pokemonList,
-  (pokemonList) => pokemonList.pokemonList,
+export const pokemonListSelector = createSelector<RootState, PokemonList, FormattedPokemonList>(
+  (state) => state.pokemonList.pokemonList,
+  (pokemonList) => {
+    return pokemonList.map((pokemonListItem) => ({
+      name: pokemonListItem.name,
+      imageUrl: getPokemonImageURL(getPokemonId(pokemonListItem.url)),
+      id: getPokemonId(pokemonListItem.url),
+    }));
+  },
 );
