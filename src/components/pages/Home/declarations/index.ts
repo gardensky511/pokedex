ccec2140eca532c3d397c43aecd17327c650ccd0
jeongmin <luvmini511@gmail.com>
2021-05-30 16:30:10 +0900

    :sparkles: getFilteredPokemonList 実装

--- src/components/pages/Home/declarations/index.ts
+++ src/components/pages/Home/declarations/index.ts
@@ -1,3 +1,14 @@
+import { SEARCH_BY_CATEGORY } from '../../../../const';
+import { EmptyStringOf } from '../../../../declarations';
+
+/**
+ * @description 基本的なオブジェクトの型
+ */
+export type BasicObject = {
+  name: string;
+  url: string;
+};
+
 /**
  * @description ポケモンリストを返すAPIのレスポンスの型
  */
@@ -19,13 +30,44 @@ export type PokemonListState = {
 /**
  * @description ポケモンリストの型
  */
-export type PokemonList = Array<{
-  name: string;
-  url: string;
-}>;
+export type PokemonList = Array<BasicObject>;
 
+/**
+ * @description 整形したポケモンリストの型
+ * @property name - ポケモンの名前
+ * @property imageUrl - 画像Url
+ * @property id - 図鑑番号
+ */
 export type FormattedPokemonList = Array<{
   name: string;
   imageUrl: string;
   id: number;
 }>;
+
+/**
+ * @description 検索カテゴリの大カテゴリの型
+ */
+export type MajorCategory = EmptyStringOf<keyof typeof SEARCH_BY_CATEGORY>;
+
+/**
+ * @description 検索カテゴリの型
+ * @property majorCategory - 大カテゴリ
+ * @property smallCategory - 小カテゴリ
+ */
+export type SearchCategory = {
+  majorCategory: MajorCategory;
+  smallCategory: Array<string>;
+};
+
+/**
+ * @description フィルタリングしてAPIから取得したポケモンリストの型
+ */
+export type GetFilteredPokemonListResponse = {
+  id: number;
+  name: string;
+  names: Array<{
+    language: BasicObject;
+    name: string;
+  }>;
+  pokemon_species: PokemonList;
+};
--- src/services/getPokemonList/index.ts
+++ src/services/getPokemonList/index.ts
@@ -1,7 +1,13 @@
 import { AXIOS } from '../logics';
-import { GetPokemonListResponse } from '../../components/pages/Home/declarations';
+import { GetFilteredPokemonListResponse, GetPokemonListResponse } from '../../components/pages/Home/declarations';
 
 /**
  * @description 全体ポケモンリスト取得
  */
 export const getPokemonList = async () => AXIOS.get<GetPokemonListResponse>('/pokemon');
+
+/**
+ * @description フィルタリングされたポケモンリスト取得
+ */
+export const getFilteredPokemonList = async (category: string) =>
+  AXIOS.get<GetFilteredPokemonListResponse>(`/${category}`);
