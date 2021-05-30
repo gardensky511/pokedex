293942166d545bab10c69db2093ab293b2c269da
jeongmin <luvmini511@gmail.com>
2021-05-30 16:34:55 +0900

    :lipstick: Home 修正

--- src/components/pages/Home/HomePresentation.tsx
+++ src/components/pages/Home/HomePresentation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { FormattedPokemonList } from './declarations';
 import { ListCard } from '../../molecules/ListCard';
+import { SearchArea } from '../../molecules/SearchArea';
 
 type Props = {
   pokemonList: FormattedPokemonList;
@@ -11,16 +12,36 @@ const List = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 15px;
-  background-color: #f1c40f;
+`;
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 70px 0;
+`;
+
+const Heading = styled.p`
+  font-size: 36px;
+  font-weight: 900;
+  text-align: center;
+  margin-bottom: 50px;
+  display: flex;
+  justify-content: center;
 `;
 
 export const HomePresentation = ({ pokemonList }: Props) => {
   return (
-    <List>
-      {pokemonList.map((pokemonListItem) => {
-        const { id, name, imageUrl } = pokemonListItem;
-        return <ListCard id={id} name={name} imageUrl={imageUrl} key={id} />;
-      })}
-    </List>
+    <Container>
+      <Heading>
+        <span>Welcome To Pokedex!</span>
+      </Heading>
+      <SearchArea />
+      <List>
+        {pokemonList.map((pokemonListItem) => {
+          const { id, name, imageUrl } = pokemonListItem;
+          return <ListCard id={id} name={name} imageUrl={imageUrl} key={id} />;
+        })}
+      </List>
+    </Container>
   );
 };
