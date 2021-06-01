7ef800c88ff40a8f302e543a2e3736299a6cb572
jeongmin <luvmini511@gmail.com>
2021-06-02 00:09:41 +0900

    :lipstick: DropDownList コンポーネント作成

---
+++ src/components/atoms/DropDownList/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import shortid from 'shortid';
+import styled from 'styled-components';
+import { COLORS } from '../../utils/styles';
+
+type Props = {
+  items: Array<string>;
+  onClick: any; // todo: 型指定する
+};
+
+export const DropDownList = ({ items, onClick }: Props) => {
+  return (
+    <Container>
+      {items.map((item) => {
+        const id = shortid.generate();
+        return (
+          <Item key={id} onClick={() => onClick(item)}>
+            {item}
+          </Item>
+        );
+      })}
+    </Container>
+  );
+};
+
+const Container = styled.ul`
+  all: unset;
+  position: absolute;
+  background-color: ${COLORS.FFF};
+  width: 100%;
+  border: 3px solid ${COLORS.BDC3C7};
+  border-radius: 8px;
+  top: calc(100% + 8px);
+  max-height: 40vh;
+  overflow: scroll;
+`;
+
+const Item = styled.li`
+  list-style: none;
+  padding: 15px;
+  cursor: pointer;
+  border-bottom: 1px solid ${COLORS.BDC3C7};
+`;
