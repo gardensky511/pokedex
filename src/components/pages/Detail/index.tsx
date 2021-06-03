f3f28afcfd470aa577d82e850e7e56585ee2e7b8
jeongmin <luvmini511@gmail.com>
2021-06-04 00:55:37 +0900

    :lipstick: ポケモン stats グラフ追加

--- src/components/pages/Detail/index.tsx
+++ src/components/pages/Detail/index.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { Radar } from 'react-chartjs-2';
+import styled from 'styled-components';
 import { GuidanceText } from '../../atoms/GuidanceText';
 import { AppDispatch, useSelector } from '../../../redux/store';
 import { fetchPokemonDetail } from '../../../redux/modules/pokemonDetail';
 import { pokemonDetailSelector } from '../../../redux/selectors/pokemonDetail';
+import { COLORS } from '../../utils/styles';
 
 // todo: Homeに戻ったとき入力してた検索条件を保持するようにする(難易度：下)
 
@@ -12,10 +15,47 @@ export const Detail = ({ match }: RouteComponentProps<{ id: string }>) => {
   const { id } = match.params;
   const dispatch: AppDispatch = useDispatch();
   const { pokemonDetail } = useSelector(pokemonDetailSelector);
+  console.log(pokemonDetail);
 
   useEffect(() => {
     dispatch(fetchPokemonDetail(Number(id)));
   }, []);
 
-  return <GuidanceText text={`It will be ${pokemonDetail.name}'s detail page`} />;
+  return (
+    <>
+      <GuidanceText text={`It will be ${pokemonDetail.name}'s detail page`} />
+      <RadarWrapper>
+        <Radar
+          type="radar"
+          data={{
+            labels: pokemonDetail.stats.map((stat) => stat.name),
+            datasets: [
+              {
+                label: `${pokemonDetail.name}'s stats`,
+                backgroundColor: COLORS.YELLOW_TRANSPARENT,
+                borderColor: COLORS.YELLOW,
+                pointBorderColor: COLORS.YELLOW,
+                pointHoverBackgroundColor: COLORS.WHITE,
+                pointHoverBorderColor: COLORS.YELLOW,
+                pointBorderWidth: 6,
+                data: pokemonDetail.stats.map((stat) => stat.baseStat),
+              },
+            ],
+          }}
+          options={{
+            r: {
+              min: 15,
+              ticks: {
+                stepSize: 15,
+              },
+            },
+          }}
+        />
+      </RadarWrapper>
+    </>
+  );
 };
+
+const RadarWrapper = styled.div`
+  width: 600px;
+`;
